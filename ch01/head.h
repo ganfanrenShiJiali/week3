d8b62acd19f07d0d013f10d1a12633940ae78f38
ganfanrenShiJiali <91459872+ganfanrenShiJiali@users.noreply.github.com>
2021-10-14 20:02:05 +0800

    Update head.h

--- ch01/head.h
+++ ch01/head.h
@@ -1,5 +1,6 @@
 #include<stdio.h>
-
+#ifndef _List_h
+#define _List_h
 struct Node;
 typedef struct Node *PtrToNode;
 typedef PtrToNode List;
@@ -14,4 +15,9 @@ struct Node
 List Initialize(List L);
 List MakeEmpty(List L);
 List Insert(ElementType a[], int N, List L);
+List Insert_null(ElementType x, List L);
 void PrintLots(List L, List P);
+List intersection(List T, List P,List U);
+List unionset(List T, List P, List U);
+void printSet(List L);
+#endif
